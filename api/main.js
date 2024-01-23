ea2aa4530e823a0a51211de0cecd39202560f2a2
Ananthan <ananthan.rajasekharan@gmail.com>
2024-01-23 16:25:26 +0530

    :tada: | inital templete

---
+++ api/main.js
@@ -0,0 +1,47 @@
+import Fastify from "fastify";
+import mongoose from "mongoose";
+import DataModel from "./dataModel.js";
+
+const fastify = Fastify({ logger: true });
+
+let uri = "mongodb+srv://ananthan:ananthan@cluster0.apq2bmf.mongodb.net/BHLTH?retryWrites=true&w=majority"
+
+mongoose.connect(uri)
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+fastify.post("/data", async (request, reply) => {
+  const existingData = await DataModel.findOne({ key: "store" });
+
+  if (existingData) {
+    existingData.data = request.body.data;
+    await existingData.save();
+  } else {
+    const data = new DataModel({ data: request.body.data });
+    await data.save();
+  }
+
+  // const data = new DataModel({ key, request.body.data, data: request.body.data });
+  // await data.save();
+
+  reply.code(201).send({message : "success"});
+});
+
+fastify.get("/data", async (request, reply) => {
+ const data = await DataModel.find();
+ let result = data[0].data;
+ reply.code(200).send({data: result});
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3001, host: "0.0.0.0" });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+start();
